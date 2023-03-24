668483b8fad380d54e0d1e3076d3549d9a5aea86
nathansabourdy <nathan.sabourdy@epitech.eu>
2023-03-24 22:55:04 +0100

    feat: just draw circles randomly

--- JAM-2/JAM-2/ContentView.swift
+++ JAM-2/JAM-2/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0..<Int.random(in: 1...2), id: \.self) { _ in
+                    let radius = CGFloat.random(in: 10...30)
+                    let x = CGFloat.random(in: radius...geometry.size.width - radius)
+                    let y = CGFloat.random(in: radius...geometry.size.height - radius)
+                    Circle()
+                        .onTapGesture {
+                            print("soala")
+                        }
+                        .foregroundColor(.blue)
+                        .frame(width: radius * 2, height: radius * 2)
+                        .position(x: x, y: y)
+                }
+            }
         }
-        .padding()
+//        Text("Count: \(count)")
+//        .onAppear {
+//            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+//                // Increment count
+//                count += 1
+//            }
+//        }
     }
 }
 
