06cef66ecfdc8d1b0b104f4585fd08ff2c1c53a3
Corent1P <corentin.piquet@epitech.eu>
2023-03-24 19:43:55 +0100

    [+] main

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,26 @@
 ** main
 */
 
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!", sf::Style::Resize);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    sf::Event event;
+
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
