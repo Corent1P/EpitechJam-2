626db2aba536268921e526dfac33bad7eda34954
KillianD1 <72009583+KillianD1@users.noreply.github.com>
2023-03-24 22:22:32 +0100

    feat: add Xcode project

---
+++ JAM-2/JAM-2/JAM_2App.swift
@@ -0,0 +1,17 @@
+//
+//  JAM_2App.swift
+//  JAM-2
+//
+//  Created by Killian Desserich on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct JAM_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
